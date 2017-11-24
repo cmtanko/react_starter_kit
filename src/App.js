7434cf4a5de1fe9c38dc42cfe41edccb3509ae08
Suchan Badyakar <suchanbadyakar@lftechnology.com>
2017-11-24 13:08:39 +0545

    basic designs added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import { Navbar, Nav, NavItem, Jumbotron, Button } from 'react-bootstrap'
 
 class App extends Component {
   render () {
     return (
       <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>
-            Welcome to Reactv123
-          </h1>
-        </header>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button className="btn btn-primary">suchan</button>
+        <Navbar collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href='#'>React-Starter-Kit</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav>
+              <NavItem eventKey={1} href='#'>Home</NavItem>
+              <NavItem eventKey={2} href='#'>Users</NavItem>
+            </Nav>
+            <Nav pullRight>
+              <NavItem eventKey={1} href='#'>Register</NavItem>
+              <NavItem eventKey={2} href='#'>Login</NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <div>
+          <Jumbotron>
+            <h1>React Starter Kit</h1>
+            <p>
+              This is a front end using react , using backend as Node.
+            </p>
+            <p><Button bsStyle='primary'>Learn more</Button></p>
+          </Jumbotron>
+        </div>
+
       </div>
     )
   }
